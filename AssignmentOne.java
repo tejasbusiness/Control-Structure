28b1df7de40c0a48edf31428e563a62438df21ba
tejasbusiness <tejasbusiness@gmail.com>
2016-10-12 23:29:53 +0530

    Add files via upload

---
+++ AssignmentOne.java
@@ -0,0 +1,12 @@
+package pkgAsignmnt;
+
+public class AssignmentOne {
+
+	public static void main(String[] args) {
+		if ( System.out.printf( "Tejas Gandhi" ) == null ) {
+			// Do Nothing
+		}
+
+	}
+
+}
